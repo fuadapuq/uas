b48f5f7727f2d5c7935b9b90bcf6d325f758a8be
fuadapuq <fuazis34@gmail.com>
2025-06-17 00:33:58 +0700

    ook

--- ziz.js
+++
@@ -1,9 +0,0 @@
-const AdmZip = require('adm-zip');
-
-// Inisialisasi ZIP
-const zip = new AdmZip("UAS.zip");
-
-// Ekstrak ke folder tujuan
-zip.extractAllTo("UAS", true); // Akan membuat folder 'UAS' jika belum ada
-
-console.log("File berhasil diekstrak ke folder UAS");
